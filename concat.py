0612224cc382164391395fd5d28eae60181a72a9
Ubuntu <ubuntu@ip-172-31-31-141.us-west-2.compute.internal>
2020-05-05 15:13:08 +0000

    this branch contains code to run on a large corpus (1GB). Specifically, I have removed the dependence of multiprocessing Pool when tokenising the corpus in this version because that requires copying the working memory resulting in  memory error

--- concat.py
+++ concat.py
@@ -23,7 +23,8 @@ import numpy
 import sys
 PATH_TO_REPSEVAL = "../repseval/src"
 sys.path.insert(0, PATH_TO_REPSEVAL)
-import eval
+from evaluate import evaluate_embed_matrix
+from wordreps import WordReps
 
 def create_signal_matrix(corpus_fname, model_config, algorithm):
     with open(model_config, "r") as f:
@@ -52,10 +53,80 @@ def estimate_pip(path):
     pip_calculator.estimate_signal()
     pip_calculator.estimate_pip_loss()
     pip_calculator.plot_pip_loss()
-    return pip_calculator        
+    return pip_calculator    
+
+def concat(source_matrices, weights_list):
+    """
+    Concatenates the given source embedding matrices. Can be used to perform both source-specific and
+    dimension-specific concatenation.
+    
+    Parameters
+    ----------
+    source_matrices: a list of numpy.ndarrays
+                    each representing n x k embeddng matrix. The vocabulary of the words n must be equal in all embedding matrices and their
+                    dimensionalities k can be different.
+        
+    weights_list: diagonal weights (numpy.ndarrays) for each source.
+                In the case of source-specific weighting all weights for a source will be equal, whereas for dimension-specific weighting they will be different.
+    
+    Returns
+    -------
+    concat: a single numpy.ndarray 
+            with the same n rows and the number of columns equals the sum of source dimensionalities.
+    """
+    assert(len(source_matrices) == len(weights_list))
+    return numpy.concatenate([source_matrices[i] @ numpy.diag(weights_list[i]) for i in range(len(source_matrices))], axis=1)
+
+
+def get_source_weighted_concat_coef(lmdas, myus, k):
+    """
+    Compute the concatenation coefficient under source-weighted concatenation.
+    
+    Parameters
+    ----------
+    lambdas : numpy.array
+        spectrum of the signal matrix.
+        
+    myus :  numpy.array
+        spectrum of the embedding matrix.
+        
+    k : int
+        rank of the embedding matrix.
+        
+    Returns
+    --------
+    c : float
+        the concatenation coefficient.
+    """
+    return numpy.dot(lmdas[:k], myus[:k]) / numpy.dot(myus[:k], myus[:k])
+
+
+def get_dimension_weighted_concat_coef(lmdas, myus, k):
+    """
+    Compute the concatenation coefficient under dimension-weighted concatenation.
+    
+    Parameters
+    ----------
+    lambdas : numpy.array
+        spectrum of the signal matrix.
+        
+    myus :  numpy.array
+        spectrum of the embedding matrix.
+        
+    k : int
+        rank of the embedding matrix.
+        
+    Returns
+    --------
+    c : numpy.array
+        the concatenation coefficient.
+    """
+    return lmdas[:k] / myus[:k]
+    
 
 def process():
-    corpus_fname = "./data/small.txt"
+    #corpus_fname = "./data/text8.zip"
+    corpus_fname = sys.argv[1]
     settings = [("glove","./config/glove_sample_config.yml"), ("word2vec", "./config/word2vec_sample_config.yml"), ("lsa", "./config/lsa_sample_config.yml")]
 
     cfg = {}
@@ -70,15 +141,47 @@ def process():
         pip_calculator[algorithm] = estimate_pip(path[algorithm])     
     
     k = {}
-    source = {}
+    sources = []
     for algo, _ in settings:
         print(algo)
         k[algo] = numpy.argmin(pip_calculator[algo].estimated_pip_loss)
-        source[algo] = signal_matrix[algo].U[:,:k[algo]] @ numpy.diag(signal_matrix[algo].spectrum[:k[algo]])
-        print(source[algo].shape)        
+        source_mat = signal_matrix[algo].U[:,:k[algo]] @ numpy.diag(signal_matrix[algo].spectrum[:k[algo]])
+        sources.append(source_mat)      
         WR = WordReps()
-        WR.load_matrix(source[algo], tokenizer[algo].dictionary)
+        WR.load_matrix(source_mat, tokenizer[algo].dictionary)
         evaluate_embed_matrix(WR, mode="lex")
+    
+    print("Unweighted concatenation...")
+    weights_list = [numpy.ones(k[algo]) for algo, _ in settings]
+    M = concat(sources, weights_list)
+    WR = WordReps()
+    WR.load_matrix(M, tokenizer[algo].dictionary)
+    evaluate_embed_matrix(WR, mode="lex")
+
+    # source-weighted concatenation
+    print("Source weighted concatenation...")
+    weights_list = []
+    for algo, _ in settings:
+        c = get_source_weighted_concat_coef(signal_matrix[algo].spectrum, 
+                                            numpy.array(pip_calculator[algo].estimated_signal), 
+                                           k[algo])
+        weights_list.append(c * numpy.ones(k[algo]))
+    M = concat(sources, weights_list)
+    WR = WordReps()
+    WR.load_matrix(M, tokenizer[algo].dictionary)
+    evaluate_embed_matrix(WR, mode="lex")
+
+    print("Dimension weighted concatenation...")
+    weights_list = []
+    for algo, _ in settings:
+        c = get_dimension_weighted_concat_coef(signal_matrix[algo].spectrum, 
+                                            numpy.array(pip_calculator[algo].estimated_signal), 
+                                           k[algo])
+        weights_list.append(c)
+    M = concat(sources, weights_list)
+    WR = WordReps()
+    WR.load_matrix(M, tokenizer[algo].dictionary)
+    evaluate_embed_matrix(WR, mode="lex")
 
 def main():
     M = numpy.random.randn(10, 50)
--- matrix/glove_matrix.py
+++ matrix/glove_matrix.py
@@ -15,6 +15,8 @@ class GloVeMatrix(SignalMatrix):
         if "skip_window" not in self._params:
             self._params["skip_window"] = 5
         self.check_params()
+        self.x_max = float(self._params["x_max"])
+        self.alpha = float(self._params["alpha"])
 
     def check_params(self):
         if isinstance(self._params["skip_window"], int) and self._params["skip_window"] > 0:
@@ -34,17 +36,25 @@ class GloVeMatrix(SignalMatrix):
             cooccurance_count[center_word_id][center_word_id] -= 1
         return cooccurance_count, vocabulary_size
 
+    def f(self, x):
+        """
+        Returns the square root of the f weight function for glove.
+        """
+        return np.log(x) * (1.0 if x > self.x_max else np.sqrt((x / self.x_max) ** self.alpha)) 
+
 
     def construct_matrix(self, data):
         cooccur, vocabulary_size = self.build_cooccurance_dict(data)
 
         Nij = np.ones([vocabulary_size, vocabulary_size])
+        fvec = np.vectorize(self.f)
         for i in range(vocabulary_size):
             for j in range(vocabulary_size):
                 Nij[i,j] += cooccur[i][j]
         with warnings.catch_warnings():
             """log(0) is going to throw warnings, but we will deal with it."""
             # c.f. Pennington et al
-            log_count = np.log(Nij)
+            log_count = np.log(Nij) 
+            #log_count = fvec(Nij)
         return log_count
 
